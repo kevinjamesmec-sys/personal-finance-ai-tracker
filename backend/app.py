b9e42ce47738fcd8af51a13e3b69c116c8203208
kevinjamesmec-sys <kevinjames.mec@gmail.com>
2026-06-19 12:10:38 +0530

    Update app.py
    Imported csv and categorize_transaction

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,6 @@
-﻿from flask import Flask, request, jsonify
+import csv
+from categorizer import categorize_transaction
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from db import get_db_connection
 
